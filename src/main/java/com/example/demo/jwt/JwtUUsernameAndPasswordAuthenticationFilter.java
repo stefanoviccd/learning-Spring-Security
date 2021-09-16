0e51aedea3f806af505c1c16f2e89edaf780aad1
dragana <stefanoviic.dragana@gmail.com>
2021-09-16 13:49:36 +0200

    Implament sending token back to the client

--- src/main/java/com/example/demo/jwt/JwtUUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/example/demo/jwt/JwtUUsernameAndPasswordAuthenticationFilter.java
@@ -1,18 +1,24 @@
 package com.example.demo.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class JwtUUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-  private final AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
 
     public JwtUUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
@@ -20,19 +26,33 @@ public class JwtUUsernameAndPasswordAuthenticationFilter extends UsernamePasswor
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-       try {
-           //read values from request and put them in UserAndPassrordRequest class
-           UsernameAndPasswordAuthenticationRequest authenticationRequest=new ObjectMapper().readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
-            Authentication authentication=new UsernamePasswordAuthenticationToken(
+        try {
+            //read values from request and put them in UserAndPassrordRequest class
+            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper().readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
                     authenticationRequest.getUsername(),
                     authenticationRequest.getPassword()
             );
             //return type is Authentication
             return authenticationManager.authenticate(authentication);
 
-       }
-       catch (IOException e){
-           throw new RuntimeException(e);
-       }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    //if authentication fails, it is not going to be executed, only if it is success
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        // we need to generate the token
+        //needs to be strong enough and really secure
+        String key = "Jgt4Aq0097bfllgy6pmjjvye20H6Jgt4Aq0097bfllgy6pmjjvye20H6";
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setExpiration(Date.valueOf(LocalDate.now().plusWeeks(2))).signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+        //send the token to the client by adding it to the response header
+        response.addHeader("Authorisation", "Bearer "+token);
+    }
 }
